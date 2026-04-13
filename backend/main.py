8375a74bd0e78ebc1603703d22ee45bc7854ed97
SBanerjee2005-sudo <soham0405@gmail.com>
2026-04-13 13:02:38 +0530

    Week 1: Backend (FastAPI) + Frontend setup

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def home():
+    return {"message": "Backend is running successfully"}
+
+# Patients API
+@app.get("/patients")
+def get_patients():
+    return [
+        {"id": 1, "name": "Soham", "age": 20},
+        {"id": 2, "name": "Suman", "age": 21}
+    ]
+
+# Doctors API
+@app.get("/doctors")
+def get_doctors():
+    return [
+        {"id": 1, "name": "Dr. Sen", "specialization": "Cardiology"},
+        {"id": 2, "name": "Dr. Roy", "specialization": "Neurology"}
+    ]
+
+# Appointments API
+@app.get("/appointments")
+def get_appointments():
+    return [
+        {"id": 1, "patient": "Soham", "doctor": "Dr. Sen"},
+        {"id": 2, "patient": "Suman", "doctor": "Dr. Roy"}
+    ]
